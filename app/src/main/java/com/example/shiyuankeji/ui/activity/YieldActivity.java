a0c0eb6b49f51e1b6eb94c47fbeb19c2aa45d319
XiaoHuisir <860663594@qq.com>
2020-07-27 16:36:06 +0800

    说明： 7.27 - 16：35 字体问题，收益自动刷新问题

--- app/src/main/java/com/example/shiyuankeji/ui/activity/YieldActivity.java
+++ app/src/main/java/com/example/shiyuankeji/ui/activity/YieldActivity.java
@@ -1,12 +1,16 @@
 package com.example.shiyuankeji.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.shiyuankeji.R;
@@ -16,7 +20,9 @@ import com.example.shiyuankeji.bean.QueryEarningsBean;
 import com.example.shiyuankeji.bean.YieDetailsBean;
 import com.example.shiyuankeji.interfaces.IBasePresenter;
 import com.example.shiyuankeji.interfaces.contract.QueryEarningsContract;
+import com.example.shiyuankeji.presenter.HomePresenter;
 import com.example.shiyuankeji.presenter.QueryEarningsPresenter;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +46,11 @@ public class YieldActivity extends BaseActivity implements QueryEarningsContract
     RelativeLayout reNoData;
     @BindView(R.id.re_have)
     RelativeLayout reHave;
+    @BindView(R.id.smartRef)
+    SwipeRefreshLayout smartRefs;
+    @BindView(R.id.scroll_view)
+    ScrollView  scrollView;
+    private boolean indxler =false;
     private ArrayList<QueryEarningsBean.ShareInfoVoBean> listyieid;
     private YieldAdapter yieldAdapter;
 
@@ -60,6 +71,7 @@ public class YieldActivity extends BaseActivity implements QueryEarningsContract
 
     @Override
     protected void initView() {
+       newrefres(true); //自动刷新
         reeYield.setHasFixedSize(true);
         reeYield.setNestedScrollingEnabled(false);
         listyieid = new ArrayList<>();
@@ -86,15 +98,67 @@ public class YieldActivity extends BaseActivity implements QueryEarningsContract
             if (shareInfoVo.size()>0&&shareInfoVo!=null){
                 reNoData.setVisibility(View.GONE);
                 reHave.setVisibility(View.VISIBLE);
+                listyieid.clear();
                 listyieid.addAll(shareInfoVo);
                 yieldAdapter.notifyDataSetChanged();
                 tvNow.setText(String.valueOf(queryEarningsBean.getTotalprice()));
                 tvZong.setText(String.valueOf(queryEarningsBean.getProfit()));
+                indxler=true;
+                newrefres(true); //自动刷新
             }else {
                 reNoData.setVisibility(View.VISIBLE);
                 reHave.setVisibility(View.GONE);
+                indxler=false;
+                newrefres(false); //自动刷新
             }
 
+        }
+    }
+
+    private void newrefres(boolean indx) {
+        if (indx==true){
+            //设置刷新球颜色
+            smartRefs.setColorSchemeColors(Color.BLUE, Color.RED, Color.YELLOW);
+            smartRefs.setProgressBackgroundColorSchemeColor(Color.parseColor("#ffffff"));//#BBFFFF
+            ViewTreeObserver obeser = smartRefs.getViewTreeObserver();
+            obeser.addOnWindowFocusChangeListener(new ViewTreeObserver.OnWindowFocusChangeListener() {
+                @Override
+                public void onWindowFocusChanged(boolean hasFocus) {
+                    smartRefs.setRefreshing(true);
+//                Toast.makeText(context, "刷新", Toast.LENGTH_SHORT).show();
+//                ToastUtil toastUtil2 = new ToastUtil(context, R.layout.ok_toast_center_horizontal, "登录成功！");
+//                toastUtil2.show();
+                    ((QueryEarningsPresenter) mPresenter).earnings();
+//                Log.i("11getMeasuredHeight",mSwipeRefreshLayout.getMeasuredHeight()+"");
+                    smartRefs.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (smartRefs != null) {
+                                smartRefs.setRefreshing(false);
+                                scrollView.fullScroll(View.FOCUS_UP);
+                            }
+                        }
+                    }, 2000);
+                }
+            });
+            smartRefs.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    ((QueryEarningsPresenter) mPresenter).earnings();
+
+                    smartRefs.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (smartRefs != null) {
+                                smartRefs.setRefreshing(false);
+                                scrollView.fullScroll(View.FOCUS_UP);
+                            }
+                        }
+                    }, 2000);
+                }
+            });
+        }else {
+            return;
         }
     }
 
